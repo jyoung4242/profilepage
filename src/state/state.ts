fd84bf50d41fba7b2abb93070d4672dcf784b205
Justin Young <justin_dean_young@yahoo.com>
2022-09-18 00:35:22 -0400

    landing page

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,1 +1,26 @@
-export let model = {};
+import mugshot from "../assets/headshot.jpeg";
+
+export let model = {
+  global: {
+    size: "large",
+    windowWidth: 800,
+    windowHeight: 600,
+    screenWidth: 800,
+    screenHeight: 600,
+    get respContent() {
+      if (model.global.size == "large") return true;
+      else return false;
+    },
+    update: function (e: any) {
+      model.global.screenWidth = window.screen.availWidth;
+      model.global.screenHeight = window.screen.availHeight;
+      model.global.windowWidth = window.innerWidth;
+      model.global.windowHeight = window.innerHeight;
+      if (model.global.windowWidth < 800 || model.global.screenWidth < 1000) model.global.size = "small";
+      else model.global.size = "large";
+    },
+  },
+  main: {
+    mug: mugshot,
+  },
+};
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,6 @@
 declare module "peasy-ui";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.wav";
+declare module "*.mp3";
